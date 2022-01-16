d3d019fe46dda095811a1b128fe0ee528e8b9f9b
Mariana Costa <mariana.costa214@gmail.com>
2022-01-16 18:51:26 +0100

    Add collision between pad and wall

--- src/BouncePad.gd
+++ src/BouncePad.gd
@@ -1,16 +1,12 @@
 extends KinematicBody2D
 
-export var speed = 200
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export var speed = 400
+var velocity = Vector2(speed, 0)
 
 func _physics_process(delta):
+	var direction = 0
 	if Input.is_action_pressed("ui_right"):
-		position.x += speed * delta
+		direction = 1
 	if Input.is_action_pressed("ui_left"):
-		position.x -= speed * delta
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		direction = -1
+	var collision_info = move_and_collide(direction * velocity * delta)
