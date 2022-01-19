ced6f4b7f6d08b5caa63dbc6b1cbb79713d2794c
Mariana Costa <mariana.costa214@gmail.com>
2022-01-19 08:27:45 +0100

    Rework brick placement logic

--- src/Bricks.gd
+++ src/Bricks.gd
@@ -2,13 +2,11 @@ extends Node2D
 
 const BRICK_RESOURCE = preload("res://Brick.tscn")
 
-const H_DIST_BETWEEN_BRICKS = 5
-const V_DIST_BETWEEN_BRICKS = 5
 const H_SCREEN_PADDING = 4
 
 var h_num_of_bricks
 var v_num_of_bricks = 5
-export var start_pos = Vector2(H_SCREEN_PADDING, 22 + 10)
+export var start_pos = Vector2(H_SCREEN_PADDING, 22 + 10) # NOTE: 22 (outside of top wall) + 10 (distance to that wall) 
 
 var width_sprite
 var height_sprite
@@ -19,27 +17,21 @@ func _ready():
 	width_sprite = 8
 	height_sprite = 2
 	screen_dimensions = get_viewport_rect().size
-	# TODO This calculation isn't correct
-	# __[   ]__
-	# I should assume a single brick is composed of the sprite width, along with the padding to the left and right of it
-	# Then, when placing the bricks, the starting position should not be the edge of the render zone and should instead
-	# take into consideration the padding to the left of the brick, as well as half of the brick's width
-	var h_num_of_bricks_float = (screen_dimensions.x - (H_SCREEN_PADDING * 2)) / (width_sprite + H_DIST_BETWEEN_BRICKS)
+	var h_num_of_bricks_float = (screen_dimensions.x - (H_SCREEN_PADDING * 2)) / width_sprite
 	h_num_of_bricks = int(round(h_num_of_bricks_float))
 	print(h_num_of_bricks)
 	spawn_bricks()
 
 func spawn_bricks():
-	var v_inc = height_sprite + H_DIST_BETWEEN_BRICKS
-	var h_inc = width_sprite + H_DIST_BETWEEN_BRICKS
+	var v_inc = height_sprite
+	var h_inc = width_sprite
 	var pos = start_pos
 	var instance
 	
 	for v in range(v_num_of_bricks):
-		for h in range(h_num_of_bricks):
-			pos.x += h_inc
+		for h in range(h_num_of_bricks + 1): # NOTE: +1 to avoid skipping one column of bricks
 			instance = BRICK_RESOURCE.instance()
 			add_child(instance)
 			instance.position = pos
+			pos.x += h_inc
 		pos = Vector2(start_pos.x, pos.y + v_inc)
-	pass
